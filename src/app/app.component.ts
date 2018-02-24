78f9a38c898811358d5115fd9b50254234f280d5
Matt Treichel <trei5470@mylaurier.ca>
2018-02-24 09:31:20 -0500

    Move company retrieval to the main app and pass it into children components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Company } from './company'
+import { CompanyService } from './company.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SECURE COUCH??';
+export class AppComponent implements OnInit {
+  company: Company;
+
+  constructor(private companyService: CompanyService) { }
+
+  ngOnInit() {
+    this.companyService.getCompany("RBC").subscribe(
+      data => this.company = {...data});
+  }
+
 }
--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,26 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Company } from '../company';
-import { CompanyService } from '../company.service'
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.scss']
 })
-export class CompanyComponent implements OnInit {
+export class CompanyComponent {
 
-  company: Company;
-
-  constructor(private companyService: CompanyService) { }
-
-  ngOnInit() {
-    this.showCompany();
-  }
-
-  showCompany() {
-    this.companyService.getCompany("RBC").subscribe(
-      data => this.company = {...data});
-  }
+  @Input() company: Company;
 
 }
