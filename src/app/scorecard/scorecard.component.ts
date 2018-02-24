955161c871467bfc0989c870f83b9cb04b38e959
Matt Treichel <trei5470@mylaurier.ca>
2018-02-24 15:00:52 -0500

    Fix javascript forever

--- src/app/scorecard/scorecard.component.ts
+++ src/app/scorecard/scorecard.component.ts
@@ -15,7 +15,7 @@ export class ScorecardComponent implements OnInit {
   constructor(private route: ActivatedRoute, private companyService: CompanyService) { }
 
   ngOnInit() {
-    const company = this.route.snapshot.paramMap.get('company');
+    let company = this.route.snapshot.paramMap.get('company');
     if (!company) company = "rbc";
     this.companyService.getCompany(company).subscribe(
       data => { console.log(data); this.company = {...data}; }
